f3279211e323acdb4ecad338501655fa4ac3b395
JaZa <47424360+JaZax@users.noreply.github.com>
2020-08-17 16:18:24 +0200

    babel plugin + convert hangman to class component

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -15,11 +15,80 @@ const images = [
 
 let game = {
     word: "javascript",
+
 }
+
+// thx!! https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript
 
 String.prototype.replaceAt = function (index, replacement) {
     return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
+
+class Hangman extends Component {
+
+    state = {
+        imgIndex: 1,
+        hiddenWord: '_'.repeat(game.word.length)
+    }
+
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleKeyDown);
+    }
+
+    nextImage = () => {
+        this.setState({ imgIndex: this.state.imgIndex + 1 })
+    }
+
+    letters = [];
+
+    handleKeyDown = (e) => {
+        console.log('key pressed!')
+
+        let pressedKey = e.key
+        let check = game.word.search(pressedKey)
+
+        if (check != -1) {
+            for (let i = -1; i < game.word.length; i++) {
+                if (game.word.charAt(i) == pressedKey) {
+                    let index = i;
+                    let hiddenWordProp = this.state.hiddenWord;
+
+                    this.letters.push({ pressedKey, index })
+
+                    this.letters.forEach(letter => {
+                        hiddenWordProp = hiddenWordProp.replaceAt(letter.index, letter.pressedKey)
+                    })
+
+                    console.log('got it')
+                    this.setState({ hiddenWord: hiddenWordProp })
+                } else {
+
+                }
+            }
+
+        } else {
+            this.nextImage()
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div id="wordToGuess">{game.word}</div>
+                <button onClick={this.nextImage}>next</button>
+
+                <div id="hiddenWord">{this.state.hiddenWord}</div>
+                <img src={images[this.state.imgIndex]}></img>
+            </>
+        )
+    }
+
+}
+/*
 
 const Hangman = () => {
     let [imgIndex, changeImgIndex] = useState(1)
@@ -32,8 +101,12 @@ const Hangman = () => {
     let letters = [];
 
     document.addEventListener('keydown', (e) => {
+        e.preventDefault()
+
         let pressedKey = e.key
         let check = game.word.search(pressedKey)
+
+        console.log('elo')
 
         if (check != -1) {
             for (let i = -1; i < game.word.length; i++) {
@@ -60,13 +133,15 @@ const Hangman = () => {
 
     return (
         <>
+            
             <div id="wordToGuess">{game.word}</div>
-            <div id="hiddenWord">{hiddenWord}</div>
             <button onClick={nextImage}>next</button>
+            
+            <div id="hiddenWord">{hiddenWord}</div>
             <img src={images[imgIndex]}></img>
         </>
     )
 
-}
+*/
 
 export default Hangman
